72fd242e4bb921d44dd47552078f3bc46e324340
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-11-25 17:08:25 -0600

    setting up the challenge object

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+let challenge = {
+    title: '',
+    difficulty: '',
+    description: '',
+    tags: '',
+    packs: '',
+    succession: {
+        inheritance: '',
+        bloodline: '',
+        heir: '',
+        species: ''
+    },
+    generations: {
+        1: {
+            title: '',
+            color: '',
+            description: '',
+            requirements: '',
+            milestones: '',
+        }
+    }
+}
