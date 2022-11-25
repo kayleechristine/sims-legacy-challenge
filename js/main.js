d2bb4ddda2bbcf54c891c038d5edbc4c9277aeda
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-11-25 17:39:49 -0600

    setting up the challenge object

--- js/main.js
+++ js/main.js
@@ -7,19 +7,30 @@ let challenge = {
     description: '',
     tags: '',
     packs: '',
-    succession: {
-        inheritance: '',
-        bloodline: '',
-        heir: '',
-        species: ''
-    },
-    generations: {
-        1: {
-            title: '',
-            color: '',
-            description: '',
-            requirements: '',
-            milestones: '',
-        }
-    }
+    // succession: {
+    //     inheritance: '',
+    //     bloodline: '',
+    //     heir: '',
+    //     species: ''
+    // },
+    // generations: {
+    //     1: {
+    //         title: '',
+    //         color: '',
+    //         description: '',
+    //         requirements: '',
+    //         milestones: '',
+    //     }
+    // }
 }
+
+$('#submit').click(function(e) {
+    e.preventDefault();
+    challenge.title = $('#challenge-title').val();
+    challenge.difficulty = $('#challenge-difficulty').val();
+    challenge.description = $('#challenge-description').val();
+    challenge.tags = $('#challenge-tags').val();
+    // challenge.packs = $('#pack-container').children('selected').val();
+    challenge.packs = $('[name="req-pack"]').val();
+    console.log(challenge);
+});
