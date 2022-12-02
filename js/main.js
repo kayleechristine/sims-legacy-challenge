e217253bcf4f21d1208efe5d8c13419ade085380
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-12-02 16:21:25 -0600

    succession rules now push into the challenge object

--- js/main.js
+++ js/main.js
@@ -1,39 +1,52 @@
 "use strict";
+(function(){
 
 // Bootstrap Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 // Challenge Data
-let challenge = {
-    title: '',
-    difficulty: '',
-    description: '',
-    tags: '',
-    packs: '',
-    succession: { // TODO: Push data to challenge object
-        inheritance: '',
-        bloodline: '',
-        heir: '',
-        species: ''
-    },
-    // generations: {
-    //     1: {
-    //         title: '',
-    //         color: '',
-    //         description: '',
-    //         requirements: '',
-    //         milestones: '',
-    //     }
-    // }
-}
+$('#submit').click(function(e) {
+    e.preventDefault();
+
+    let packs = [];
+    $.each($('input[name="req-pack"]:checked'), function() {
+        packs.push($(this).val());
+    })
+
+    let challenge = {
+        overview: {
+            title: $('#challenge-title').val(),
+            difficulty: $('#challenge-difficulty').val(),
+            description: $('#challenge-description').val(),
+            tags: $('#challenge-tags').val().split(' '),
+            packs
+        },
+        succession: {
+            inheritance: $('#inheritance-law').val(),
+            bloodline: $('#bloodline-law').val(),
+            heir: $('#heir-law').val(),
+            species: $('#species-law').val()
+        },
+        // generations: { // TODO: Push generation data to challenge object
+        //     1: {
+        //         title: '',
+        //         color: '',
+        //         description: '',
+        //         requirements: '',
+        //         milestones: '',
+        //     }
+        // }
+    }
+    console.log(challenge);
+});
 
 // Challenge Tags
 // TODO: Stylize how tags appear in the input field
 let input = document.getElementById('challenge-tags');
-input.addEventListener('keydown', addHash, false);
+input.addEventListener('keydown', hashtag, false);
 
-function addHash(e) {
+function hashtag(e) {
     if (e.keyCode === 32 && e.target.value.length) {
         e.preventDefault();
 
@@ -51,17 +64,4 @@ function addHash(e) {
     }
 }
 
-// Submit - Generate Challenge
-$('#submit').click(function(e) {
-    e.preventDefault();
-    challenge.title = $('#challenge-title').val();
-    challenge.difficulty = $('#challenge-difficulty').val();
-    challenge.description = $('#challenge-description').val();
-    challenge.tags = $('#challenge-tags').val().split(' ');
-    let packs = [];
-    $.each($('input[name="req-pack"]:checked'), function() {
-        packs.push($(this).val());
-    })
-    challenge.packs = packs;
-    console.log(challenge);
-});
+})();
