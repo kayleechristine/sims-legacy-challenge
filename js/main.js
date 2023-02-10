145f7c4a145c3421fd3a24bcb93e3c7fafca5985
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-02-09 23:04:17 -0600

    adjusting minor formatting

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 (function(){
 // TODO: incorporate mySQL & Java III
-    //
 
 // Bootstrap Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
