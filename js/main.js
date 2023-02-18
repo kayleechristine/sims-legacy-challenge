f840e5d4534c609a5d74de00588c95ed9a904085
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-02-18 15:35:09 -0600

    adjusting minor formatting

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 (function(){
 // TODO: incorporate mySQL & Java III
 
-
 // Bootstrap Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
